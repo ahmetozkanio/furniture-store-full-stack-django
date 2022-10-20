13ec44b28bdbff14f55287e15cfc3faddd764fb3
ahmetozkanio <bledwarrior@gmail.com>
2022-10-20 20:37:48 +0300

    product details

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 
+from product.models import Product, ProductImage, SliderProduct
+
 # Create your views here.
 
 def index(request):
+    slider_products = SliderProduct.objects.all()
     context = {
-        "numbers": [1, 2, 3, 4, 5, 6]
+        "slider_products": slider_products
     }
     return render(request, "index.html", context)
 
 def products(request):
+    product_list = Product.objects.all()
+    images = ProductImage.objects.all()
     context = {
-        "numbers": [1, 2, 3, 4, 5, 6]
+        'products' : product_list,
+        'images' : images,
+    }
+    return render(request, "products.html", context)
+
+def product_detail(request,slug):
+    product_list = Product.objects.all()
+    images = ProductImage.objects.all()
+    context = {
+        'products' : product_list,
+        'images' : images,
     }
     return render(request, "products.html", context)
