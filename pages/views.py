c3c7b028e4c6c1355f6fb31d6e828824bce21b14
ahmetozkanio <bledwarrior@gmail.com>
2022-10-25 22:40:06 +0300

    language

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from product.models import Product, ProductImage, SliderProduct
 
+from django.utils import translation
 # Create your views here.
 
 def index(request):
@@ -20,12 +21,33 @@ def products(request):
     }
     return render(request, "products.html", context)
 
-def product_detail(request,slug):
+def product_detail(request, slug):
+
+    cur_language = translation.get_language()
+
+
+
+
+
     product = Product.objects.get(slug = slug)
-    
+    print('HELLOOOOO')
+    print(slug)
     images = ProductImage.objects.all().filter(product = product)
+    
+    # languages = dict(settings.LANGUAGES).keys()
+    # q = Q()
+    # for lang in languages:
+    #     kwargs = {'slug_%s' % lang: slug}
+    #     q |= Q(**kwargs)
+    # articles = Product.objects.filter(slug = slug)
+    # if articles.exists():
+    #     article = articles.first()
+    # else:
+    #     return Http404
+    # return reverse('product_detail', kwargs={'slug': slug})
     context = {
         'product' : product,
         'images' : images,
     }
     return render(request, "product_detail.html", context)
+
