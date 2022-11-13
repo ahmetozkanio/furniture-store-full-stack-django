68d48315aa2660f8b0ebfe0c1b73629c46e7eaa3
ahmetozkanio <bledwarrior@gmail.com>
2022-11-13 19:44:55 +0300

    heroku configure

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn donaco.wsgi
--- donaco/settings.py
+++ donaco/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c81u79#v7$k*volt(0hp%k^%sv+#zb@s5%7%0@6-t!o0_-0l2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['donaco.herokuapp.com']
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+       "whitenoise.middleware.WhiteNoiseMiddleware",#heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +61,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 ROOT_URLCONF = 'donaco.urls'
 
 TEMPLATES = [
