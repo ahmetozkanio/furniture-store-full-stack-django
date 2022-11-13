674e971ee4bbb85f9c27688a76b0362875db3f0b
ahmetozkanio <bledwarrior@gmail.com>
2022-11-13 19:49:44 +0300

    static

--- donaco/urls.py
+++ donaco/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
      path("set_language/<str:language>", set_language, name="set-language"),    
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
